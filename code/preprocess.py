953b82c910e60af8d9861c6a8bd30b868ef5593d
sgallon <shcmsgallon@outlook.com>
2021-01-16 13:52:58 +0800

    preprocess done

--- code/preprocess.py
+++ code/preprocess.py
@@ -4,93 +4,37 @@
 # @Author      : sgallon
 # @Email       : shcmsgallon@outlook.com
 # @File        : preprocess.py
-# @Description :
+# @Description : load data from raw file, change them into (node-weighted & undirected) graph and save them
 
 
-import random
-import json
-import numpy as np
-import scipy.io as sio
-import networkx as nx
-import matplotlib.pyplot as plt
 from util.util import listdir_remove_temp
 from util.GraphLoader import GraphLoaderMat, GraphLoaderTxt, GraphLoaderJson
 
 
 DATA_DIR = "../data/"
+GRAPH_DIR = "../graph/"
 RANDOM_SEED = 123
 RANDOM_WEIGHT_INTERVAL = (1, 200)
+FILETYPE_DICT = {"mat": GraphLoaderMat, 'json': GraphLoaderJson, 'txt': GraphLoaderTxt}
+# FILETYPE_DICT = {"mat": GraphLoaderMat, 'json': GraphLoaderJson}
 
+def load_raw_and_save_graph(loadfile: str, savefile: str):
+    filename, filetype = loadfile.split('/')[-1].split('.')
+    if filetype not in FILETYPE_DICT.keys():
+        # raise TypeError("Filetype {} incorrect! Must be in {}".format(filetype, FILETYPE_DICT.keys()))
+        print("Filetype {} incorrect! Must be in {}".format(filetype, FILETYPE_DICT.keys()))
+        return
+    graphloader = FILETYPE_DICT[filetype]()
+    graphloader.load_graph_from_raw(loadfile, random_seed=RANDOM_SEED, weight_interval=RANDOM_WEIGHT_INTERVAL)
+    graphloader.save_graph(savefile)
 
-file_list = listdir_remove_temp(DATA_DIR)
-
-
-def load_mat(filename):
-    """
-    load SSMC .mat file
-    returns two list of nodes representing edges
-    """
-    data = sio.loadmat(filename)
-    mat = data.get("Problem")["A"][0][0]
-    x = mat.nonzero()
-    from_node_list = x[0].tolist()
-    to_node_list = x[1].tolist()
-    return from_node_list, to_node_list
-
-
-def make_weighted_graph_with_from_to_node_list(from_node_list, to_node_list, random_seed=RANDOM_SEED):
-    """
-    create a weighted undirected graph from two list of nodes representing edges
-    randomly assign weight to each node if add_weight==True
-    """
-    assert len(from_node_list) == len(to_node_list), "from_node_list and to_node_list must have the same length!"
-    random.seed(random_seed)
-    G = nx.Graph()
-    node_set = set(from_node_list).union(set(to_node_list))
-    weighted_node_list = [(node, {"weight": random.randint(*RANDOM_WEIGHT_INTERVAL)}) for node in node_set]
-    G.add_nodes_from(weighted_node_list)
-    num_edges = len(from_node_list)
-    for i in range(num_edges):
-        G.add_edge(from_node_list[i], to_node_list[i])
-    return G
-
-
-def load_json(filename):
-    """
-    load .json file
-    returns two list of nodes representing edges
-    """
-    pass
-
-
-def draw_graph(G, pos=None):
-    if not pos:
-        pos = nx.spring_layout(G)
-    nx.draw(G, pos)
-    # plt.savefig("./G.png")
-    plt.show()
-
-
-#################### TEST ####################
-def test_draw():
-    G = nx.petersen_graph()
-    # plt.subplot(121)
-    nx.draw(G, with_labels=True, font_weight='bold')
-    # plt.subplot(122)
-    nx.draw_shell(G, nlist=[range(5, 10), range(5)], with_labels=True, font_weight='bold')
-    plt.show()
-#################### END TEST ####################
 
 def main():
-    print("foo")
+    file_list = listdir_remove_temp(DATA_DIR)
+    for idx, file in enumerate(file_list):
+        print("------ <{:^3d}> ------".format(idx))
+        load_raw_and_save_graph(DATA_DIR + file, GRAPH_DIR + file.split('.')[0] + ".graph")
 
 
 if __name__ == "__main__":
-    fnode_list, tnode_list = load_mat(DATA_DIR + "GD98_c.mat")
-    G = make_weighted_graph_with_from_to_node_list(fnode_list, tnode_list)
-    pos = nx.spring_layout(G)
-    draw_graph(G, pos)
-    # draw_graph(G, pos)
-    np.save("G.graph", G)
-    # test_draw()
     main()
--- code/util/GraphLoader.py
+++ code/util/GraphLoader.py
@@ -18,55 +18,82 @@ import scipy.io as sio
 
 class GraphLoader(metaclass=abc.ABCMeta):
     """abstract class for load data and generate weighted graph"""
+    _cls_filetype = "graph"
+
     def __init__(self):
         self._filetype = None
         self.graph = None
         self.data = None
 
     @classmethod
-    def load_get_graph(cls, file) -> nx.Graph:
-        """load graph from file and return it"""
+    def _check_file_type_cls(cls, file: str):
+        in_filetype = file.split(".")[-1]
+        if in_filetype != cls._cls_filetype:
+            raise TypeError("Expect file type {}, but got {}. \nCurrent filename is {}"
+                            .format(cls._cls_filetype, in_filetype, os.path.abspath(file)))
+
+    @classmethod
+    def get_graph_from_graph(cls, file: str) -> nx.Graph:
+        """
+        read graph from saved .graph file and return it
+        class method, does not save the graph into object
+        """
+        cls._check_file_type_cls(file)
+        graph = joblib.load(file)
         print("Successfully loaded graph from {}".format(os.path.abspath(file)))
-        return joblib.load(file)
+        cls.print_graph_info(graph)
+        return graph
 
-    def get_graph(self) -> nx.Graph:
-        """returns a networkx.Graph G"""
-        return self.graph
+    @classmethod
+    def print_graph_info(cls, graph):
+        print("#######\nGraph info:\nNodes: {}\nEdges: {}\n######".format(len(graph.nodes), len(graph.edges)))
 
-    def load_graph(self, file):
-        """load graph from file"""
+    def _check_file_type(self, file: str):
+        in_filetype = file.split(".")[-1]
+        if in_filetype != self._filetype:
+            raise TypeError("Expect file type {}, but got {}. \nCurrent filename is {}"
+                            .format(self._filetype, in_filetype, os.path.abspath(file)))
+
+    def load_graph_from_graph(self, file: str):
+        """load graph from .graph file"""
+        self._check_file_type_cls(file)
         self.graph = joblib.load(file)
         print("Successfully loaded graph from {}".format(os.path.abspath(file)))
+        self.print_graph_info(self.graph)
 
-    def save_graph(self, file):
+    def load_graph_from_raw(self, file: str, random_seed: int, weight_interval):
+        self._check_file_type(file)
+        self._load_raw_data(file)
+        print("Successfully loaded raw data from file {}".format(os.path.abspath(file)))
+        self._make_graph(random_seed, weight_interval)
+        print("Successfully generated graph!")
+        self.print_graph_info(self.graph)
+
+    def save_graph(self, file: str):
         """save graph to file"""
         joblib.dump(self.graph, file)
         print("Successfully saved graph to {}".format(os.path.abspath(file)))
 
     @abc.abstractmethod
-    def load_data(self, file: str):
+    def _load_raw_data(self, file: str):
         raise NotImplementedError
 
     @abc.abstractmethod
-    def make_graph(self, random_seed, weight_interval):
+    def _make_graph(self, random_seed: int, weight_interval):
         raise NotImplementedError
 
 
-# TODO: GraphLoader
 class GraphLoaderMat(GraphLoader):
     """class for load data and generate weighted graph from SSMC format .mat file"""
     def __init__(self):
         super().__init__()
         self._filetype = "mat"
 
-    def load_data(self, file):
+    def _load_raw_data(self, file):
         """
         load SSMC .mat file
         returns two lists of nodes representing edges
         """
-        in_filetype = file.split(".")[-1]
-        if in_filetype != self._filetype:
-            raise TypeError("Expect file type {}, but got {}.".format(self._filetype, in_filetype))
         data = sio.loadmat(file)
         mat = data.get("Problem")["A"][0][0]
         x = mat.nonzero()
@@ -75,24 +102,22 @@ class GraphLoaderMat(GraphLoader):
         if len(from_node_list) != len(to_node_list):
             raise ValueError("from_node_list and to_node_list must have the same length!")
         self.data = (from_node_list, to_node_list)
-        print("Successfully loaded data from file {}".format(os.path.abspath(file)))
 
-    def make_graph(self, random_seed, weight_interval):
+    def _make_graph(self, random_seed, weight_interval):
         """
         create a weighted undirected graph from two list of nodes representing edges
         randomly assign weight to each node
         """
         from_node_list, to_node_list = self.data
         random.seed(random_seed)
-        g = nx.Graph()
         node_set = set(from_node_list).union(set(to_node_list))
         weighted_node_list = [(node, {"weight": random.randint(*weight_interval)}) for node in node_set]
+        g = nx.Graph()
         g.add_nodes_from(weighted_node_list)
         num_edges = len(from_node_list)
         for i in range(num_edges):
             g.add_edge(from_node_list[i], to_node_list[i])
         self.graph = g
-        print("Successfully generated graph!")
 
 
 class GraphLoaderTxt(GraphLoader):
@@ -101,36 +126,78 @@ class GraphLoaderTxt(GraphLoader):
         super().__init__()
         self._filetype = "txt"
 
-    def load_data(self, file: str):
+    def _load_raw_data(self, file: str):
+        node_set = set()
+        edge_list = []
+        with open(file, 'r', encoding='utf-8') as handler:
+            for line in handler:
+                if line.strip().startswith("#"):
+                    continue
+                # use int instead of string as node name to reduce saved size
+                node_i, node_j = map(int, line.strip().split())
+                node_set.add(node_i)
+                node_set.add(node_j)
+                edge_list.append((node_i, node_j))
+        self.data = (node_set, edge_list)
 
-        self.data = 1
-        pass
-
-    def make_graph(self, random_seed, weight_interval):
-        pass
+    def _make_graph(self, random_seed, weight_interval):
+        node_set, edge_list = self.data
+        random.seed(random_seed)
+        weighted_node_list = [(node, {"weight": random.randint(*weight_interval)}) for node in node_set]
+        g = nx.Graph()
+        g.add_nodes_from(weighted_node_list)
+        g.add_edges_from(edge_list)
+        self.graph = g
 
 
 class GraphLoaderJson(GraphLoader):
-    """class for load data and generate weighted graph from .json file"""
+    """
+    class for load data and generate weighted graph from .json file
+    original data description: https://github.com/evelinag/StarWars-social-network/tree/master/networks
+    """
     def __init__(self):
         super().__init__()
         self._filetype = "json"
 
-    def load_data(self, file: str):
-        self.data = 1
-        pass
+    def _load_raw_data(self, file: str):
+        with open(file, 'r', encoding='utf-8') as handler:
+            json_record = json.loads(handler.read())
+        node_dict_list = json_record['nodes']
+        edge_dict_list = json_record['links']
+        node_list = [(node_dict['name'], {"weight": node_dict['value'], "colour": node_dict["colour"]})
+                     for node_dict in node_dict_list]
+        index_edge_list = [(edge_dict['source'], edge_dict['target']) for edge_dict in edge_dict_list]  # [(1, 2), ...]
+        node_name_list = [t[0] for t in node_list]
+        named_edge_list = []  # [("Foo", "Bar"), ...]
+        for edge in index_edge_list:
+            node_i_name = node_name_list[edge[0]]
+            node_j_name = node_name_list[edge[1]]
+            named_edge_list.append((node_i_name, node_j_name))
+        self.data = (node_list, named_edge_list)
 
-    def make_graph(self, random_seed, weight_interval):
-        pass
+    def _make_graph(self, random_seed=None, weight_interval=None):
+        node_list, edge_list = self.data
+        g = nx.Graph()
+        g.add_nodes_from(node_list)
+        g.add_edges_from(edge_list)
+        self.graph = g
 
 
 if __name__ == "__main__":
-    fname = "../../data/GD98_c.mat"
-    gname = "../../graph/GD98_c.graph"
-    glm = GraphLoaderMat()
-    glm.load_data(fname)
-    glm.make_graph(123, (1, 200))
-    G = glm.get_graph()
-    glm.save_graph(gname)
-    GG = GraphLoader.load_get_graph(gname)
+    # fname = "../../data/GD98_c.mat"
+    # gname = "../../graph/GD98_c.graph"
+    # graphloader = GraphLoaderMat()
+    # graphloader.load_raw_data(fname)
+    # graphloader.make_graph(123, (1, 200))
+    # G = graphloader.get_graph()
+    fname = "../../data/ca-AstroPh.txt"
+    gname = "../../graph/ca-AstroPh.graph"
+    graphloader = GraphLoaderTxt()
+    # fname = "../../data/starwars-full-interactions.json"
+    # gname = "../../graph/starwars-full-interactions.graph"
+    # graphloader = GraphLoaderJson()
+    graphloader.load_graph_from_raw(fname, 123, (1, 200))
+    G = graphloader.graph
+    graphloader.save_graph(gname)
+    GG = GraphLoader.get_graph_from_graph(gname)
     print("foo")
